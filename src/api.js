b51099d74f8ee4592c01518645854b4574563744
JiinSeok <seokjiin1073@gmail.com>
2024-05-22 22:26:11 +0900

    refactor: 정렬(order)이 바뀔 때마다 데이터를 새로 받아오도록 수정

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
-export async function getReviews() {
-  const response = await fetch("https://learn.codeit.kr/0220/film-reviews/");
+export async function getReviews(order = "createdAt") {
+  const query = `order=${order}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/0220/film-reviews/?${query}`
+  );
   const body = response.json();
   return body;
 }
--- src/components/App.js
+++ src/components/App.js
@@ -14,14 +14,14 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id); // id가 일치하지 않는 요소로 새로운 배열
     setItems(nextItems);
   };
-  const handleLoad = async () => {
-    const { reviews } = await getReviews(); // 리스폰스 body의 reviews 값을 destructuring. 비동기로 리스폰스 보내고, 도착하면 reviews 변수 지정
+  const handleLoad = async (orderQuery) => {
+    const { reviews } = await getReviews(orderQuery); // 리스폰스 body의 reviews 값을 destructuring. 비동기로 리스폰스 보내고, 도착하면 reviews 변수 지정
     setItems(reviews); // state 변경 -> 렌더링을 위해 App 컴포넌트 함수 재실행
   };
 
   useEffect(() => {
-    handleLoad();
-  }, []); // 두 번째 아규먼트로 빈 배열 전달하면 첫 아규먼트 콜백함수는 처음 렌더링할 때만 실행됨.
+    handleLoad(order);
+  }, [order]); // 두 번째 아규먼트로 빈 배열 디펜던시 리스트 전달하면 첫 아규먼트 콜백함수는 첫 렌더링 끝나면 비동기 실행됨. 두 번째부터는 디펜던시 리스트를 비교해서 달라진 경우에만 실행하기 때문임. - 특정 값이 바뀔 때마다 실행할 수도 있다.
 
   return (
     // 4. onDelete는 App.js에서 정의된 handleDelete 함수로 연결되어 있으며, item.id를 인자로 받습니다.
