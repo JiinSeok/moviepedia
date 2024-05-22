17ad96380f373b39a7773a34ff9d610c1b9c77e5
JiinSeok <seokjiin1073@gmail.com>
2024-05-22 23:47:04 +0900

    feat: 더 보기 버튼 추가

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
 import { useEffect, useState } from "react";
 
+const LIMIT = 6;
+
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]); // a, b는 객체
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -14,13 +19,23 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id); // id가 일치하지 않는 요소로 새로운 배열
     setItems(nextItems);
   };
-  const handleLoad = async (orderQuery) => {
-    const { reviews } = await getReviews(orderQuery); // 리스폰스 body의 reviews 값을 destructuring. 비동기로 리스폰스 보내고, 도착하면 reviews 변수 지정
-    setItems(reviews); // state 변경 -> 렌더링을 위해 App 컴포넌트 함수 재실행
+  const handleLoad = async (options) => {
+    const { reviews, paging } = await getReviews(options); // 리스폰스 body의 reviews 값을 destructuring. 비동기로 리스폰스 보내고, 도착하면 reviews 변수 지정
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]); // 추가로 데이터 불러올 땐 items 배열에 가져온 요소 추가
+    }
+    setOffset(options.offset + reviews.length); // state 변경 -> 렌더링을 위해 App 컴포넌트 함수 재실행 // 변경 offset = 현재 데이터 로드의 시작 위치 + 이번에 가져온 리뷰 데이터 개수 = 다음 번 데이터 로드가 시작될 위치
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
   };
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]); // 두 번째 아규먼트로 빈 배열 디펜던시 리스트 전달하면 첫 아규먼트 콜백함수는 첫 렌더링 끝나면 비동기 실행됨. 두 번째부터는 디펜던시 리스트를 비교해서 달라진 경우에만 실행하기 때문임. - 특정 값이 바뀔 때마다 실행할 수도 있다.
 
   return (
@@ -30,6 +45,9 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>별점순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button disabled={!hasNext} onClick={handleLoadMore}>
+        더 보기
+      </button>
     </div>
   );
 }
