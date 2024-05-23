e4d10add435a69ff7ddd6cfb2b82c9c874a24735
JiinSeok <seokjiin1073@gmail.com>
2024-05-23 21:17:40 +0900

    refactor: reviewForm 관련 state, 함수 통합

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,29 +2,37 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((preValues) => ({
+      ...preValues,
+      [name]: value, // 표현식 [name의 값으로 프로퍼티명 지정하고 value에 해당하는 값 지정 가능.
+    }));
   };
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0; // false면 오른쪽
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
 
   return (
     // onChange js 사용자 입력 끝났을 때, React-jsx 사용자가 입력할 때마다
-    <form className='ReviewForm'>
-      <input value={title} onChange={handleTitleChange} />
-      <input type='number' value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+    <form className='ReviewForm' onSubmit={handleSubmit}>
+      <input name='title' value={values.title} onChange={handleChange} />
+      <input
+        name='rating'
+        type='number'
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name='content' value={values.content} onChange={handleChange} />
+      <button type='submit'>확인</button>
     </form>
   );
 }
